b63899ee2107af77d8d66dc4c426cbee9cf9d804
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-14 11:17:26 +0600

    [ADD] Text utility css added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Hotch potch</h1>
+      <h1 className="">Hotch potch</h1>
       <p>Regurlar text</p>
     </main>
   );
