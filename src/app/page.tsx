2eb23bfc7c7612fe9e75901700e4987d42860472
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-14 11:55:14 +0600

    [ADD] Assets added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="main-container">
       <h1 className="">Hotch potch</h1>
       <p>Regurlar text</p>
     </main>
