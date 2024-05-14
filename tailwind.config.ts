bb85f9393a69ab04050fcc1dd84005e3a51469fb
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-14 10:59:23 +0600

    Project init & colors & fonts configured

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+        outfit: ["var(--font-outfit)", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#12141D",
+          foreground: "#000000",
+        },
+        seondary: {
+          DEFAULT: "#FAFAFA",
+          foreground: "#B7B8BB",
+        },
+        accent: {
+          DEFAULT: "#0EA5E9",
+        },
+        gradient: {
+          DEFAULT: "#2B59FF",
+          foreground: "#BB2BFF",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
