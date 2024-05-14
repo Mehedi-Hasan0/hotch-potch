e7a931fba9b9d4c83f151e5c928bb3f4eb50c54c
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-14 20:20:21 +0600

    [ADD] Custom & reusable button with gsap animation added

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,12 +1,31 @@
+import CustomButton from "@/components/ui/CustomButton";
 import Logo from "./Logo";
 import NavLink from "./NavLink";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="main-container flex-center gap-5 py-6 2xl:py-8">
-      <div className="flex items-center gap-5">
+      <div className="flex items-center gap-7">
         <Logo />
         <NavLink />
+      </div>
+      {/* btn */}
+      <div className="flex-center gap-4">
+        <Link href={"/"}>
+          <CustomButton key={1} textLabel="Sign in" />
+        </Link>
+        <Link href={"/"}>
+          <CustomButton
+            key={2}
+            textLabel="Get started free"
+            btnBgColor="primary-gradient"
+            borderColor="border-none"
+            hoverColor1="bg-[#FFC947]"
+            hoverColor2="bg-accent"
+            hoverColor3="bg-primary"
+          />
+        </Link>
       </div>
     </header>
   );
