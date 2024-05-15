7a938da252db1c3dcd753c4f28b5dee74f184157
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-15 23:24:00 +0600

    [FIX] company heading size issu fixed

--- src/components/Company.tsx
+++ src/components/Company.tsx
@@ -2,8 +2,8 @@ import ComLogo from "./ComLogo";
 
 export default function Company() {
   return (
-    <div className="flex flex-col items-center justify-center gap-6 md:gap-8 lg:gap-12 xl:gap-14 2xl:gap-16 section-padding main-container">
-      <h3 className="title-heading font-outfit font-medium">
+    <div className="flex flex-col items-center justify-center gap-6 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-14 2xl:gap-16 section-padding main-container">
+      <h3 className="text-xl md:text-xl lg:text-lg xl:text-xl 2xl:text-2xl font-outfit font-medium text-center">
         Trusted by nearly 5000+ paying customers
       </h3>
       <ComLogo />
